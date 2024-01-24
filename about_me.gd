4da1ebbf758fe5aae61e68584e84f17d4a382f27
MrPurdy <143815144+MrPurdy@users.noreply.github.com>
2024-01-24 13:34:14 -0600

    Initial commit

---
+++ about_me.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_favsub_pressed():
+	$Label.text = "Global Studies"
+
+
+func _on_name_pressed():
+	$Label.text = "Cameron Purdy"
+
+
+func _on_about_pressed():
+	$Label.text = "I'm 15 I play football, workout, and play variety of video games."
