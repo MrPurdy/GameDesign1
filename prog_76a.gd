8d383830bdbd1fb1971d9155fd5e94041ba9b05e
MrPurdy <143815144+MrPurdy@users.noreply.github.com>
2024-01-31 13:15:37 -0600

    Update

--- prog_76a.gd
+++ prog_76a.gd
@@ -9,3 +9,22 @@ func _on_btn_calc_pressed():
 	var num3 = int($num3.text)
 	var Ans1 = num1 * num2
 	var Ans2 = Ans1 * num3
+	$Ans1.text = "Answer: " + str(Ans1)
+	$Ans2.text = "Final Answer: " + str(Ans2)
+	
+	
+	
+
+func _on_btn_clear_pressed():
+	$Ans1.text = "Answer: "
+	$Ans2.text = "Final Answer: "
+	$num1.text = ""
+	$num2.text = ""
+	$num3.text = ""
+	
+	
+	
+	
+
+func _on_btn_exit_pressed():
+	get_tree().quit()
