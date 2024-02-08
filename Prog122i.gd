3452ad576249bf45ef1d5e53acc622eb5c435f12
MrPurdy <143815144+MrPurdy@users.noreply.github.com>
2024-02-08 13:43:43 -0600

    Update

--- Prog122i.gd
+++ Prog122i.gd
@@ -6,7 +6,15 @@ extends Control
 func _on_btn_calc_pressed():
 	for lcv in range (-25, 26):
 		var cube = lcv**3
-		var cuber = lcv**3 
-		var line = str(lcv) + "     " + str(cube) + "     " + str(cuber) + "     "
+		var cuber = lcv**(1.0/3.0)
+		var line = str(lcv) + "     " + str(cuber) + "     " + str(cube) + "     "
 		$ItemList.add_item(line)
 
+
+
+func _on_btn_clear_pressed():
+	$ItemList.clear()
+
+
+func _on_btn_exit_pressed():
+	get_tree().quit()
