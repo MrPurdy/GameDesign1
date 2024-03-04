78d1f0159a4a96e0c8bc1bc54966464007f3a833
MrPurdy <143815144+MrPurdy@users.noreply.github.com>
2024-03-04 10:05:04 -0600

    Update

--- change_lvl.gd
+++ change_lvl.gd
@@ -4,5 +4,5 @@ extends Area2D
 
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
-		var lvl = "res://" + next_level + ".tscn"
+		var lvl = "res://" + next_level + ""
 		get_tree().change_scene_to_file(lvl)
