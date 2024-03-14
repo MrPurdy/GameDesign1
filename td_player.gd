fa9202cc20eeaa2fec7307a61f4e85506ced0edd
MrPurdy <143815144+MrPurdy@users.noreply.github.com>
2024-03-14 13:42:54 -0500

    Update 3/14

--- td_player.gd
+++ td_player.gd
@@ -17,6 +17,7 @@ func _physics_process(delta):
 		Input.get_axis("ui_left", "ui_right"),
 		Input.get_axis("ui_up", "ui_down")
 	)
+	update_animation(direction)
 	if direction.length() > 0:
 		look_direction = direction
 		direction = direction.normalized()
@@ -29,3 +30,20 @@ func _physics_process(delta):
 	
 	if Input.is_action_just_pressed("ui_cancel"):
 		menu_instance.show()
+
+
+func update_animation(direction):
+	var a_name = "idle_"
+	if direction.length() > 0:
+		look_direction = direction
+		a_name = "walk_"
+	if look_direction.x != 0:
+		a_name += "side"
+		$AnimatedSprite2D.flip_h = look_direction.x < 0
+	elif look_direction.y < 0:
+		a_name += "up"
+	elif look_direction.y > 0:
+		a_name += "down"
+		
+	$AnimatedSprite2D.animation = a_name
+	$AnimatedSprite2D.play()
