88de60b8a3a801dc7635c71071500ec84b1b6a88
MrPurdy <143815144+MrPurdy@users.noreply.github.com>
2024-03-25 13:32:28 -0500

    Update 3/25

--- PlayerHud.gd
+++ PlayerHud.gd
@@ -16,10 +16,10 @@ func create_heart():
 
 
 func draw_hearts():
-	for hearts in hearts.getchildren():
-		hearts.remove_child(hearts)
+	for heart in hearts.get_children():
+		hearts.remove_child(heart)
 	for i in range(int(player.data.max_health) / 20):
-		create_heart()
+		create_heart()  # 1 heart per 20 hp
 
 func _ready():
 	draw_hearts()
@@ -28,19 +28,18 @@ func _process(delta):
 	var p_health = player.data.health
 	var full_hearts = floor(p_health / 20)
 	var remainder = int(p_health) % 20
+	
 	for heart in hearts.get_children():
 		var index = heart.get_index()
 		var x = (index % HEART_ROW_SIZE) * HEART_OFFSET
 		var y = (index / HEART_ROW_SIZE) * HEART_OFFSET
-		heart.position = Vector2(x,y)
+		heart.position = Vector2(x, y)
 		
-		
+		# Frame 8 empty, 7 1/4, 6 half, 5 3/4, 4 full
 		if index > full_hearts:
 			heart.frame = 8
 		elif index == full_hearts:
 			heart.frame = 8 - int(remainder / 5)
 		elif index < full_hearts:
 			heart.frame = 4
-			
-	
 	pass
