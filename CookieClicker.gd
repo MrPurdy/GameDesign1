d1ffa1b1d8a6f7174616c10ece0458e4ca4c5969
MrPurdy <143815144+MrPurdy@users.noreply.github.com>
2024-04-29 13:43:35 -0500

    Update

--- CookieClicker.gd
+++ CookieClicker.gd
@@ -2,7 +2,16 @@ extends Node2D
 
 
 
+var cursors = 0
+var cookies = 0.0
+func _on_cookie_clicked_pressed():
+	cookies += 1.0
+	$BigCookie/lblCookies.text = str(cookies) + " Cookies"
 
-func _on_button_pressed():
-	var cookies = 0 + 1
-	$BigCookie/lblCookies.text = str(cookies) + "Cookies" 
+func _on_timer_timeout_funcname() -> void:
+	queue_free()
+
+func _on_buy_cursor_pressed():
+	cursors += 1
+	
+
