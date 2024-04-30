cf18d52914b9f3f694b4f3b385cf06cd0b332b6a
MrPurdy <143815144+MrPurdy@users.noreply.github.com>
2024-04-30 13:43:37 -0500

    Update 4/30

--- CookieClicker.gd
+++ CookieClicker.gd
@@ -1,17 +1,24 @@
 extends Node2D
 
 
-
+var costC = 15
 var cursors = 0
 var cookies = 0.0
 func _on_cookie_clicked_pressed():
 	cookies += 1.0
 	$BigCookie/lblCookies.text = str(cookies) + " Cookies"
 
-func _on_timer_timeout_funcname() -> void:
-	queue_free()
 
 func _on_buy_cursor_pressed():
-	cursors += 1
-	
+	costC * 1.5
+	$BigCursor/BuyCursor.text = "Cost: " + str(costC) + " Cookies"
+	if cookies >= costC:
+		cursors += 1
+		$BigCursor/mnyCursors.text = str(cursors)
+	cookies - costC
+	$Timer.start(1)
 
+
+func _on_timer_timeout():
+	cookies += 0.1
+	$BigCookie/lblCookies.text = str(cookies) + " Cookies"
