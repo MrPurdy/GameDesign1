33fa3673955fda4a245482f3ccc1838e4af83be6
MrPurdy <143815144+MrPurdy@users.noreply.github.com>
2024-05-01 13:50:11 -0500

    Update 5/1

--- CookieClicker.gd
+++ CookieClicker.gd
@@ -4,18 +4,22 @@ extends Node2D
 var costC = 15
 var cursors = 0
 var cookies = 0.0
+
 func _on_cookie_clicked_pressed():
 	cookies += 1.0
-	$BigCookie/lblCookies.text = str(cookies) + " Cookies"
+	$BigCookie/lblCookies.text = str(round(cookies)) + " Cookies"
 
 
 func _on_buy_cursor_pressed():
-	costC * 1.5
-	$BigCursor/BuyCursor.text = "Cost: " + str(costC) + " Cookies"
 	if cookies >= costC:
+		cookies -= costC
 		cursors += 1
+		costC *= 1.15
+		$BigCursor/costC.text = "Cost: " + str(round(costC)) + " Cookies"
 		$BigCursor/mnyCursors.text = str(cursors)
-	cookies - costC
+	if cookies < 0:
+		cookies = 0
+	
 	$Timer.start(1)
 
 
